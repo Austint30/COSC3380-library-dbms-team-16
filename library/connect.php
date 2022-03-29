ea57b73b07feb4f752ee63511ca711ad31694ccf
Zaid Kidwai <92498192+ZaidKidwai@users.noreply.github.com>
2022-03-28 21:02:53 -0500

    XAMPP htdocs contents
    3/28/2022

---
+++ library/connect.php
@@ -0,0 +1,18 @@
+<?php
+	
+	$host = 'team16librarydb.cytnyqknpuhd.us-east-1.rds.amazonaws.com';
+	$user = 'admin';
+	$pass = 'team16libraryuh';
+	$db_name = 'library'; 
+	
+	$conn = new mysqli($host, $user, $pass, $db_name);
+	if($conn->connect_error){
+		die('Connection error: '.$conn->connect_error);
+	}
+	
+    $result = $conn->query("SELECT `First Name` FROM library.Account WHERE Type = 'ADMIN'");
+    $results = $result->fetch_all();
+    print_r($results);
+    $conn->close();
+
+?>	
