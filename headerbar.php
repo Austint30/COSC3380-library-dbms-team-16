a576f6d521d38b3f50306e0e9ed2b8df20e7d26f
Austin Thibodeaux <austinthibodeaux@gmail.com>
2022-04-01 00:17:41 -0500

    Added color to header and other stuff

--- headerbar.php
+++ headerbar.php
@@ -1,9 +1,17 @@
-<nav class="navbar navbar-light bg-light">
+<style>
+    .library-headerbar {
+        background-color: #1e8b1e !important;
+        box-shadow: 0 1px 7px 1px rgba(0,0,0,0.45);
+        -webkit-box-shadow: 0 1px 7px 1px rgba(0,0,0,0.45);
+        -moz-box-shadow: 0 1px 7px 1px rgba(0,0,0,0.45);
+    }
+</style>
+<nav class="navbar navbar-dark bg-dark library-headerbar">
   <div class="container-fluid">
-    <a class="navbar-brand">Navbar</a>
+    <a class="navbar-brand">Team 16 Library System</a>
     <form class="d-flex">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
+      <button class="btn btn-outline-light" type="submit">Search</button>
     </form>
   </div>
 </nav>
