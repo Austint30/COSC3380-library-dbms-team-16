b17c330cd668b90565697fe019a2ad21b94d4b28
Zaid Kidwai <92498192+ZaidKidwai@users.noreply.github.com>
2022-04-03 14:30:16 -0500

    Changed Index.php to be a most popular page
    The books chosen are hard coded for now

--- index.php
+++ index.php
@@ -5,130 +5,115 @@
         <?php include 'bootstrap.php'; 
 		include 'connect.php';?>	
     </head>
-<!----------------------Here we have the book title table----------------------------------------->
-    <body>
-	    <?php include 'headerbar-auth.php' ?>		
-		<?php
-			$result = $conn->query("SELECT * FROM library.`Book Title`");
-			$columns = $result->fetch_fields();
-			$results = $result->fetch_all();
-		?>
+<!----------------------Here we have the popular books----------------------------------------->
+	<body>
+		<?php include 'headerbar-auth.php' ?>		
 		<div class="container mt-5">
-            <h1>Books</h1>
+			<h1>Our Most Popular Books</h1>
 			<table class="table table-hover table-striped">
 				<thead>
 					<tr>
-						<?php
-							foreach($columns as $colData){
-								echo "<th>$colData->name</th>";
-							}
-						?>
-						<th>Learn More</th>
+						<th scope="col">ISBN</th>
+						<th scope="col">Title</th>
+						<th scope="col">Genre</th>
+						<th scope="col">AuthorFName</th>
+						<th scope="col">AuthorLName</th>
+						<th scope="col">AuthorMName</th>
+						<th scope="col">Replacement Cost</th>
+						<th scope="col">DDN</th>
+						<th scope="col">Year Published</th>
+						<th scope="col">Learn More</th>
 					</tr>
-				<thead>
+				</thead>
 				<tbody>
-					<?php
-						foreach($results as $row){
-							echo "<tr>";
-							for ($i = 0; $i < count($row); $i++) {
-								$value = $row[$i];
-								echo "<td>$value</td>";
-							}
-							echo "<td>
-                                    <a href='#' class='btn btn-primary btn-small' style='float: left;'>
-                                        Learn More
-                                    </a>
-                                </td>";
-							echo "</tr>";
-						}
-					?>
+					<tr>
+						<th scope="row">9780048230911</th>
+						<td>The Lord of the Rings</td>
+						<td>FANTASY</td>
+						<td>John</td>
+						<td>Tolkien</td>
+						<td>Ronald Reuel</td>
+						<td>18</td>
+						<td></td>
+						<td>1955</td>
+						<td><button type="button" class="btn btn-primary">Check It Out</button></td>
+					</tr>
+					<tr>
+						<th scope="row">9780048230911</th>
+						<td>The Lord of the Rings</td>
+						<td>FANTASY</td>
+						<td>John</td>
+						<td>Tolkien</td>
+						<td>Ronald Reuel</td>
+						<td>18</td>
+						<td></td>
+						<td>1955</td>
+						<td><button type="button" class="btn btn-primary">Check It Out</button></td>
+					</tr>
+					<tr>
+						<th scope="row">9780048230911</th>
+						<td>The Lord of the Rings</td>
+						<td>FANTASY</td>
+						<td>John</td>
+						<td>Tolkien</td>
+						<td>Ronald Reuel</td>
+						<td>18</td>
+						<td></td>
+						<td>1955</td>
+						<td><button type="button" class="btn btn-primary">Check It Out</button></td>
+					</tr>
 				</tbody>
 			</table>
 		</div>
-<!-------------------------------Here we have the media title table-------------------------------->
-
-		<?php
-			$result = $conn->query("SELECT * FROM library.`Media Title`");
-			$columns = $result->fetch_fields();
-			$results = $result->fetch_all();
-		?>
-		<div class="container">
-            <h1 class="mt-5">Media</h1>
-			<table class="table table-striped table-hover">
+<!-------------------------------Here we have the popular media-------------------------------->
+<div class="container mt-5">
+			<h1>Our Most Popular Media</h1>
+			<table class="table table-hover table-striped">
 				<thead>
 					<tr>
-						<?php
-							foreach($columns as $colData){
-								echo "<th>$colData->name</th>";
-							}
-						?>
-						<th>Learn More</th>					
+						<th scope="col">Media ID</th>
+						<th scope="col">Title</th>
+						<th scope="col">Year Published</th>
+						<th scope="col">Author</th>
+						<th scope="col">Genre</th>
+						<th scope="col">Data Added</th>
+						<th scope="col">Replacement Cost</th>
+						<th scope="col">Learn More</th>
 					</tr>
-				<thead>
+				</thead>
 				<tbody>
-					<?php
-						foreach($results as $row){
-							echo "<tr>";
-							for ($i = 0; $i < count($row); $i++) {
-								$value = $row[$i];
-								echo "<td>$value</td>";
-							}
-							echo "<td>
-                                    <a href='#' class='btn btn-primary btn-small' style='float: left;'>
-                                        Learn More
-                                    </a>
-                                </td>";
-							echo "</tr>";
-						}
-					?>
-				</tbody>
-			</table>
-		</div>	
-<!--------------------------------Here we have the device title table------------------------------->
-
-		<?php
-			$result = $conn->query("SELECT * FROM library.`Device Title`");
-			$columns = $result->fetch_fields();
-			$results = $result->fetch_all();
-			$conn->close();
-		?>
-		<div class="container">
-            <h1 class="mt-5">Devices</h1>
-			<table class="table table-striped table-hover">
-				<thead>
 					<tr>
-						<?php
-							foreach($columns as $colData){
-								echo "<th>$colData->name</th>";
-							}
-						?>
-						<th>Learn More</th>
 					</tr>
-				<thead>
-				<tbody>
-					<?php
-						foreach($results as $row){
-							echo "<tr>";
-							for ($i = 0; $i < count($row); $i++) {
-								$value = $row[$i];
-								echo "<td>$value</td>";
-							}
-							echo "<td>
-                                    <a href='#' class='btn btn-primary btn-small' style='float: left;'>
-                                        Learn More
-                                    </a>
-                                </td>";
-							echo "</tr>";
-						}
-					?>
-					
+					<tr>
+					</tr>
+					<tr>
+					</tr>
 				</tbody>
 			</table>
 		</div>
-
-
+<!--------------------------------Here we have the popular devices------------------------------->
+<div class="container mt-5">
+			<h1>Our Most Popular Devices</h1>
+			<table class="table table-hover table-striped">
+				<thead>
+					<tr>
+						<th scope="col">Model Number</th>
+						<th scope="col">Name</th>
+						<th scope="col">Manufacturer</th>
+						<th scope="col">Date Added</th>
+						<th scope="col">Replacement Cost</th>
+						<th scope="col">Type</th>
+						<th scope="col">Learn More</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 <!-----------------------Would like it to look more like this---------------------------------------->
-	
+		
 		<!-- <div>
 			<p class="fs-1">Wanna add buttons to side like this:</p>
 			<table class="table table-hover">
