db10c583079303858cafc02f90a8c4ff8b9ede47
Austin Thibodeaux <austinthibodeaux@gmail.com>
2022-04-07 16:33:54 -0500

    Changed connect.php to use SQL Server

--- connect.php
+++ connect.php
@@ -1,5 +1,8 @@
 <?php
-	
+	// --------------------------------------------------------------------------------
+	// Old MySQL connection code
+	// --------------------------------------------------------------------------------
+	/*
 	$host = 'team16librarydb.cytnyqknpuhd.us-east-1.rds.amazonaws.com';
 	$user = 'admin';
 	$pass = 'team16libraryuh';
@@ -9,5 +12,35 @@
 	if($conn->connect_error){
 		die('Connection error: '.$conn->connect_error);
 	}
+	*/
 
+	// --------------------------------------------------------------------------------
+	// New SQL Server conection code
+	// --------------------------------------------------------------------------------
+	$host = 'tcp:library-dbms-ms.cytnyqknpuhd.us-east-1.rds.amazonaws.com,1433';
+	$user = 'admin';
+	$pass = 'team16libraryuh';
+	$db_name = 'library';
+
+	$connectionOptions = array("Database"=>$db_name,"Uid"=>$user, "PWD"=>$pass);
+	$conn = sqlsrv_connect($host, $connectionOptions);
+
+	if($conn == false){
+		die("Failed to connect to SQL Server");
+	}
+
+	// --------------------------------------------------------------------------------
+	// Example user accounts query using SQL Server
+	// --------------------------------------------------------------------------------
+	/*
+	$sql = "SELECT [User ID], [Last Name], [First Name] FROM library.Account";
+	$stmt = sqlsrv_query( $conn, $sql );
+	if( $stmt === false) {
+		die( print_r( sqlsrv_errors(), true) );
+	}
+	
+	while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+		echo $row['User ID'].", ".$row['Last Name'].", ".$row['First Name'].'<br />';
+	}
+	*/
 ?>	
