77690470c632f0bb22f8328ff14906ed499ac2f3
Austin Thibodeaux <austinthibodeaux@gmail.com>
2022-04-15 10:26:38 -0500

    Added ability for admins to add new users

---
+++ admin-create-user-response-server.php
@@ -0,0 +1,45 @@
+<?php
+    include 'connect.php';
+    include 'require-signup.php';
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $middleName = $_POST['middleName'] ?? '';
+        $password = $_POST["password"];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $type = $_POST['type'];
+        
+        echo "POST received. Values are below:";
+        echo $firstName;
+        echo $lastName;
+        echo $middleName;
+        echo $password;
+        echo $email;
+        echo $phone;
+        echo $type;
+
+        $result = sqlsrv_query($conn,"
+            INSERT INTO library.library.Account (library.library.Account.[First Name], library.library.Account.[Last Name], library.library.Account.[Middle Name], library.library.Account.Password, library.library.Account.Email, library.library.Account.Phone, library.library.Account.Type, library.library.Account.Approved)
+            OUTPUT INSERTED.[User ID] AS [New User ID]
+            VALUES (?, ?, ?, ?, ?, ?, ?, 1);
+        ", array($firstName, $lastName, $middleName, $password, $email, $phone, $type));
+
+        if ($result){
+            $e = sqlsrv_errors();
+            $eMsg = $e[0][2];
+
+            header("Location: users.php?errormsg=Failed to sign up due to an error. Error: $eMsg");
+        }
+
+        $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_NUMERIC);
+        $userId = $row[0];
+        if ($result){
+            header("Location: users.php?msg=User successfully created.");
+        }
+    }
+    else
+    {
+        $result = "YOU SHOULDN'T BE HERE!";
+    }
+?>
--- users.php
+++ users.php
@@ -16,9 +16,57 @@
     <body>
         <?php include 'headerbar-auth.php' ?>
         <div class="container mt-5">
-            <h1 class="mb-3">User Management</h1>
+            <div class="mb-3 d-flex">
+                <h1 class="mb-0">User Management</h1>
+                <button class="btn btn-primary ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+                    Add user
+                </button>
+            </div>
             <?php include 'messages.php' ?>
-            <div class="card mb-4">
+            <div class="collapse collapse-vertical" id="collapseWidthExample">
+                <form class="card mb-3" action="admin-create-user-response-server.php" method="post">
+                    <div class="card-body">
+                        <h5 class="card-title">Add new account</h5>
+                        <div class="row align-items-start">
+                            <div class="col-6 mb-3">
+                                <label for="newuser-firstname" class="form-label">First name</label>
+                                <input class="form-control" id="newuser-firstname" name="firstName" required>
+                            </div>
+                            <div class="col-6 mb-3">
+                                <label for="newuser-lastname" class="form-label">Last name</label>
+                                <input class="form-control" id="newuser-lastname" name="lastName" required>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="newuser-password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="newuser-password" name="password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="newuser-type" class="form-label">Account type</label>
+                            <select id="newuser-type" class="form-select" name="type" required>
+                                <option value="" selected>Choose an account type</option>
+                                <option value="ADMIN">Admin</option>
+                                <option value="STAFF">Staff</option>
+                                <option value="STUDENT">Student</option>
+                                <option value="FACULTY">Faculty</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label id="newuser-email-label" for="newuser-email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="newuser-email" disabled name="email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label id="newuser-phone-label" for="newuser-phone" class="form-label">Phone</label>
+                            <input type="phone" class="form-control" id="newuser-phone" disabled name="phone" required>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <button id="newuser-button" type="submit" class="btn btn-primary" disabled>Add account</button>
+                            <div class="form-text ms-3">Users created using this form are automatically approved.</div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="card mb-3">
                 <div class="card-body">
                     <h5 class="card-title">Users awaiting approval</h5>
                 </div>
@@ -100,4 +148,40 @@
             </div>
         </div>
     </body>
+    <?php include 'scripts.php' ?>
+    <script>
+        const typeField = document.getElementById("newuser-type");
+        const newuserButton = document.getElementById("newuser-button");
+
+        function onTypeClicked(){
+            console.log("Account type changed.");
+            const emailField = document.getElementById("newuser-email");
+            const phoneField = document.getElementById("newuser-phone");
+            const emailLabel = document.getElementById("newuser-email-label");
+            const phoneLabel = document.getElementById("newuser-phone-label");
+
+            let selectValue = typeField.options[typeField.selectedIndex];
+            if (selectValue.value === "STUDENT"){
+                emailLabel.innerHTML = "Parent/Guardian email";
+                phoneLabel.innerHTML = "Parent/Guardian phone";
+            }
+            else
+            {
+                emailLabel.innerHTML = "Email";
+                phoneLabel.innerHTML = "Phone";
+            }
+            if (selectValue.value){
+                emailField.removeAttribute("disabled");
+                phoneField.removeAttribute("disabled");
+                newuserButton.removeAttribute("disabled");
+            }
+            else
+            {
+                emailField.setAttribute("disabled", true);
+                phoneField.setAttribute("disabled", true);
+                newuserButton.setAttribute("disabled", true);
+            }
+        }
+        typeField.onchange = onTypeClicked;
+    </script>
 </html>
